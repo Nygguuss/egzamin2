63bce9e314f11d4d9994dfce4ae43df1ee321013
Nygguuss <100948681+Nygguuss@users.noreply.github.com>
2023-03-29 09:01:39 +0200

    Dodaj pliki projektów.

---
+++ egzamin2/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace egzamin2
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Image> images = new List<Image>();
+        Gallery gallery;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            images.Add(new Image { Source = "pig.jpg" });
+            images.Add(new Image { Source = "minion.jpg" });
+            images.Add(new Image { Source = "truck.jpg" });
+            images.Add(new Image { Source = "cat.jpg" });;
+            gallery = new Gallery(images);
+        }
+
+
+
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            Switch thisSender = (Switch)sender;
+            if(thisSender.IsToggled)
+            {
+                Color myRgbColor = new Color();
+                myRgbColor = Color.FromRgb(24, 42, 212);
+                mybac.BackgroundColor = myRgbColor;
+            }
+            else
+            {
+                Color myRgbColor = new Color();
+                myRgbColor = Color.FromRgb(0, 76, 64);
+                mybac.BackgroundColor = myRgbColor;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            gallery.MovePrevious();
+            miejsce_zdj.Source = gallery.GetCurrentImage().Source;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            gallery.MoveNext();
+            miejsce_zdj.Source = gallery.GetCurrentImage().Source;
+        }
+    }
+}
---
+++ egzamin2/class/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Text;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+namespace egzamin2
+{
+    public class Gallery
+    {
+        private List<Image> images;
+        private int currentImageIndex;
+
+        public Gallery(List<Image> images)
+        {
+            this.images = images;
+            currentImageIndex = 0;
+        }
+
+        public Image GetCurrentImage()
+        {
+            return images[currentImageIndex];
+        }
+
+        public void MoveNext()
+        {
+            currentImageIndex = (currentImageIndex + 1) % images.Count;
+        }
+
+        public void MovePrevious()
+        {
+            currentImageIndex = (currentImageIndex - 1 + images.Count) % images.Count;
+        }
+    }
+
+}
